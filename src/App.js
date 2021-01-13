bd2da49aab9724f3bd2f89e2e39babbddaa12ef7
Nikolay <samoil.nikolai@gmail.com>
2021-01-13 11:55:58 +0200

    navigation/routers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Auth from './containers/Auth/Auth';
 import Quiz from './containers/Quiz/Quiz';
+import QuizCreator from './containers/QuizCreator/QuizCreator';
+import QuizList from './containers/QuizList/QuizList';
 import Layout from './hoc/Layout/Layout';
-
-
 
 class App extends Component {
 	render() {
 		return (
 			<Layout>
-				<Quiz/>
+				<Switch >
+					<Route path='/auth' component={Auth} />
+					<Route path='/quiz-creator' component={QuizCreator} />
+					<Route path='/quiz/:id' component={Quiz} />
+					<Route path='/' component={QuizList} />
+				</Switch>
 			</Layout>
 		);
 	}
