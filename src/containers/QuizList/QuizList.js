4552ecffb4947356b4ce45c573e9b290e8217218
Nikolay <samoil.nikolai@gmail.com>
2021-01-19 19:32:16 +0200

    added axios + connected firebase database

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,33 +1,37 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import classes from './QuizList.module.css';
+import axios from 'axios';
 class QuizList extends Component {
+	renderQuizes() {
+		return [1, 2, 3].map((quiz, index) => {
+			return (
+				<li key={index}>
+					<NavLink to={'/quiz/' + quiz}>Тест {quiz}</NavLink>
+				</li>
+			);
+		});
+	}
 
-    renderQuizes() {
-        return [1, 2, 3].map((quiz, index) => {
-            return (
-                <li key={index}>
-                    <NavLink to={'/quiz/' + quiz}>
-                        Тест {quiz}
-                    </NavLink>
-                </li>
-            );
-        });
-    }
+	componentDidMount() {
+		axios
+			.get(`https://react-quiz-a81c4-default-rtdb.firebaseio.com/quiz.json`)
+			.then((response) => {
+                console.log(response);
+            });
+	}
 
-    render() {
-        return (
-            <div className={classes.QuizList}>
-                <div>
-                    <h1>Список тестов</h1>
+	render() {
+		return (
+			<div className={classes.QuizList}>
+				<div>
+					<h1>Список тестов</h1>
 
-                    <ul>
-                        {this.renderQuizes()}
-                    </ul>
-                </div>
-            </div>
-        );
-    }
+					<ul>{this.renderQuizes()}</ul>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default QuizList;
