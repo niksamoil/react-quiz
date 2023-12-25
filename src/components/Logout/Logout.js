de5114fdce37179c44908e599ed89273d53d825b
niksamoil <samoil.nikolai@gmail.com>
2023-12-25 15:08:12 +0200

    Update eslint configuration and refactor Logout component

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -4,22 +4,18 @@ import { Redirect } from 'react-router-dom';
 import { logout } from '../../store/actions/authAction';
 
 class Logout extends Component {
-    componentDidMount() {
-        this.props.logout();
-    }
-    
-    render() {
-        return (
-           <Redirect to='/' />
-        );
-    }
+  componentDidMount() {
+    const { logout: logoutAction } = this.props;
+    logoutAction();
+  }
+
+  render() {
+    return <Redirect to="/" />;
+  }
 }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        logout: () => dispatch(logout())
-    };
-};
+const mapDispatchToProps = (dispatch) => ({
+  logout: () => dispatch(logout()),
+});
 
-export default connect(null, mapDispatchToProps) (Logout);
-
+export default connect(null, mapDispatchToProps)(Logout);
